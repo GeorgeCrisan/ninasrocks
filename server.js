79e551f74b3dc4ab1deac18cb4fe49589b9f0c96
GeorgeCrisan <georgerdp@gmail.com>
2019-05-26 19:30:22 +0100

    handle errors

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const eventLog = require("morgan");
-const handleError = require("./serverfiles/handleError");
+const path = require('path');
+const helmet = require('helmet');
+const session = require('express-session');
+const customErrors = require("./serverfiles/customErrors");
 
 //Init app
 const app = express();
@@ -12,14 +15,31 @@ const app = express();
 require('dotenv').config();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(cors());
-app.use(eventLog('dev'));
+app.use(cors()); 
+app.use(eventLog('dev')); 
 
 //Handle errors
-app.use(handleError);
+
+/// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Sorry route Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({'errors': {
+    status: err.status || 500,
+    message: err.message,
+    error: {}
+  }});
+});
+
+
 
 //Set up server
 const port = process.env.NODE_ENV === 'production' ? 80 : process.env.PORT;
-const server = app.listen(port, ()=> {
+const server = app.listen(port, ()=> { 
   console.log(`Server running on port  ${port}` );
 });
